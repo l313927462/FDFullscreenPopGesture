7348166b70267fc0466a33e8b479256c05b4f43d
Paranoid Magua <313927462@qq.com>
2020-09-18 11:22:46 +0800

    Update UINavigationController+FDFullscreenPopGesture.h

--- FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h
+++ FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h
@@ -59,4 +59,7 @@
 /// gesture. 0 by default, which means it will ignore this limit.
 @property (nonatomic, assign) CGFloat fd_interactivePopMaxAllowedInitialDistanceToLeftEdge;
 
+- (void)fd_viewWillDisappear:(BOOL)animated;
+- (void)fd_viewWillAppear:(BOOL)animated;
+
 @end
